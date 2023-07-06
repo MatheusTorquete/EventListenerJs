5331366e6f2c05c991cd4c55b4dd4e7b49788e16
Matheus <matheuswe10@gmail.com>
2023-07-06 20:07:09 -0300

    event listener botão

--- aula-event-listener-eventos-mouse/script.js
+++ aula-event-listener-eventos-mouse/script.js
@@ -1,10 +1,28 @@
-let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
-let valorSubtotal = document.getElementById("valor-subtotal");
+// let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+// let valorSubtotal = document.getElementById("valor-subtotal");
+// let subtotalInfo = {
+//   quantidade: 1,
+//   valor: 11.66,
+// };
+// quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+// valorSubtotal.innerText = subtotalInfo.valor;
 
-let subtotalInfo = {
-  quantidade: 1,
-  valor: 11.66,
-};
 
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
+// array dos produtos
+const produtos = [
+  {
+    nome: "Dom Casmurro",
+    valor: 11.99
+  }
+]
+
+// Capturando Elementos
+const addButton = document.getElementById("btn-adicionar-produto-01");
+const input = document.getElementById("quantidade-produto-01");
+
+
+
+// Eventos
+addButton.addEventListener("click", ()=> {
+  input.value = Number(input.value) + 1
+});
